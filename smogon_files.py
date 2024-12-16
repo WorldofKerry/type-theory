ff7bd3b7e40096ca3cfed8ff912009cdb95a1c22
Kerry Wang <kerrywang369@gmail.com>
2024-12-15 18:45:21 -0800

    pretty good team finder, but need to include levitate ability

--- smogon_files.py
+++ smogon_files.py
@@ -3,20 +3,20 @@ Parses Smogon / Pokemon Showdown team files
 """
 
 from pokeapi_api import get_type
-from relationships import Team
+from relationships import MultiType, Team
 
-def parse_team_file(file_path: str) -> Team:
+def parse_team_file(file_path: str) -> list[MultiType]:
     with open(file_path) as f:
         lines = f.readlines()
 
-    member_types = set()
+    member_types = list()
     prev_blank = True
     for line in lines:
         if prev_blank:
             prev_blank = False
             pokemon_name = line.strip().split(" ")[0]
-            member_types.add(get_type(pokemon_name))
+            member_types.append(get_type(pokemon_name))
         if line == "\n":
             prev_blank = True
 
-    return Team.from_list(list(member_types))
+    return member_types
--- test_smogon_files.py
+++ test_smogon_files.py
@@ -1,5 +1,20 @@
+import itertools
+from relationships import Team
 from smogon_files import parse_team_file
 
 def test_parse_team_file():
-    team = parse_team_file("team.txt")
-    print(team)
+    types = parse_team_file("team.txt")
+    teams = set()
+
+    for ts in itertools.combinations(types, 6):
+        team = Team.from_list(ts)
+        weaknesses = team.weaknesses_count()
+        resistances = team.resistances_count()
+        for weakness, count in weaknesses.items():
+            if weakness not in resistances or count > resistances[weakness] + 1:
+                break
+        else:
+            teams.add(team)
+
+    for team in teams:
+        print(team)
