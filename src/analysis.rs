dcfcbae9010bb67832ee9e6e73afcb0a67f9e3ec
Kerry Wang <kerrywang369@gmail.com>
2024-12-19 23:30:47 -0800

    minor

--- src/analysis.rs
+++ src/analysis.rs
@@ -144,13 +144,13 @@ pub fn resistance_complements(poke1: &Pokemon, poke2: &Pokemon) -> i32 {
         if r2 > 0 {
             // println!("{t:?} {r1:?} {r2:?}");
             if r1 > 0 {
-                match (r2, r1) {
-                    (2, 2) => score -= 4,
-                    (1, 2) => score -= 3,
-                    (2, 1) => score -= 2,
-                    (1, 1) => score -= 1,
-                    _ => panic!("{r1:?} {r2:?}"),
-                }
+                // match (r2, r1) {
+                //     (2, 2) => score -= 4,
+                //     (1, 2) => score -= 3,
+                //     (2, 1) => score -= 2,
+                //     (1, 1) => score -= 1,
+                //     _ => panic!("{r1:?} {r2:?}"),
+                // }
             } else {
                 score -= r1;
             }
@@ -171,6 +171,50 @@ pub fn resistance_connector(
             (poke3.clone(), score1, score2)
         })
         .collect()
+}
+
+fn compute_cyclic_resistance_complement(team: &Vec<Pokemon>) -> i32 {
+    let mut score = 0;
+    for (poke1, poke2) in team.iter().tuple_windows() {
+        score += resistance_complements(poke1, poke2);
+    }
+    score += resistance_complements(team.last().unwrap(), team.first().unwrap());
+    score
+}
+
+fn best_complement_cycle(team: &Vec<Pokemon>, pool: &Vec<Pokemon>, size: usize) -> Vec<Pokemon> {
+    if team.len() > 2 {
+        return team.clone();
+    }
+    let last_pokemon = team.last().unwrap().clone();
+    // Find candidates that complement the last pokemon
+    let candidates = pool
+        .iter()
+        .unique()
+        .map(|p| (p.clone(), resistance_complements(&last_pokemon, p)))
+        .max_set_by_key(|(_, s)| *s)
+        .into_iter()
+        .inspect(|(p, s)| {
+            println!("{p:?} {s:?}");
+        })
+        .map(|(p, _)| p)
+        .collect::<Vec<_>>();
+    // Score how well first pokemon complements each candidate, and teams built from there
+    let mut best_team = vec![];
+    let mut best_score = i32::MIN;
+    for candidate in candidates {
+        let new_team = team.iter().cloned().chain(vec![candidate]).collect();
+        let score1 = compute_cyclic_resistance_complement(&new_team);
+        let score2 =
+            compute_cyclic_resistance_complement(&best_complement_cycle(&new_team, pool, size));
+        let score = std::cmp::max(score1, score2);
+        println!("{score:?} {new_team:?}");
+        if score > best_score {
+            best_score = score;
+            best_team = new_team;
+        }
+    }
+    best_team
 }
 
 #[cfg(test)]
@@ -276,16 +320,22 @@ mod test {
     #[test]
     fn recursive_search_specific_type_complements() {
         use BasicType::*;
-        let root = Pokemon::from((Steel, Flying));
-        for _ in 0..1 {
-            let best_complements = Pokemon::all()
-                .into_iter()
-                .unique()
-                .map(|p| (p.clone(), resistance_complements(&root, &p)))
-                .max_set_by_key(|(_, s)| *s);
-            best_complements.iter().for_each(|(p, _)| {
-                println!("{p:?} {:?}", resistance_complements(&p, &root));
+        let team = best_complement_cycle(&vec![Pokemon::from((Steel, Flying))], &Pokemon::all(), 2);
+        println!("{team:?}");
+    }
+
+    #[test]
+    fn resistance_complements_test() {
+        use BasicType::*;
+        let poke = Pokemon::from((Water, Ground));
+        // Pokemon::all()
+        vec![Pokemon::from((Flying, Fairy))]
+            .into_iter()
+            .unique()
+            .map(|p| (p.clone(), resistance_complements(&poke, &p)))
+            .sorted_by(|(_, s1), (_, s2)| s1.cmp(s2))
+            .for_each(|(p, s)| {
+                println!("{s:?} {p:?}");
             });
-        }
     }
 }
