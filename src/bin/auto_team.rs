e5f064c3ff3294aa561e909813116c32e9169a7b
Kerry Wang <kerrywang369@gmail.com>
2024-12-21 23:43:31 -0800

    pretty good rayon performance

--- src/bin/auto_team.rs
+++ src/bin/auto_team.rs
@@ -2,7 +2,7 @@
 use core::f64;
 use itertools::Itertools;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use type_theory::analysis::autoscale::AutoScale;
 use type_theory::analysis::{offensive_coverage, resistance, simulated_annealing};
 use type_theory::pokemon::Pokemon;
@@ -18,7 +18,7 @@ pub fn score<const N: usize>(team: &Vec<Pokemon>) -> [f64; N] {
 
 fn main() {
     let size = 5;
-    let autoscale = Mutex::new(AutoScale::new([1.0, 0.25, 0.5, 0.75]));
+    let autoscale_global = Mutex::new(AutoScale::new([1.0, 0.25, 0.5, 0.75]));
     let pool = Pokemon::all();
     let pool = {
         let pool = Pokemon::from_pkhex_dump("data/Box Data Dump.csv");
@@ -27,47 +27,42 @@ fn main() {
         pool
     };
 
-    let mut best_team = Pokemon::random_team(&pool, size);
-    autoscale.lock().unwrap().add(score(&best_team));
+    let best_team = Arc::new(Mutex::new(Pokemon::random_team(&pool, size)));
+    autoscale_global.lock().unwrap().add(score(&best_team.lock().unwrap()));
 
-    loop {
-        let results: Vec<(AutoScale<4>, Vec<Pokemon>)> = (0..16)
-            .into_par_iter()
-            .map(|_| {
-                let mut autoscale = autoscale.lock().unwrap().clone();
-                let team = simulated_annealing(
-                    Pokemon::random_team(&pool, size),
-                    &pool,
-                    &mut autoscale,
-                    score,
-                );
-                (autoscale, team)
-            })
-            .collect();
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(8)
+        .build_global()
+        .unwrap();
 
-        let mut autoscale = autoscale.lock().unwrap();
+    rayon::iter::repeat(()).for_each(|_| {
+        let mut autoscale = autoscale_global.lock().unwrap().clone();
+        let team = simulated_annealing(
+            Pokemon::random_team(&pool, size),
+            &pool,
+            &mut autoscale,
+            score,
+        );
+        autoscale_global.lock().unwrap().combine(&autoscale);
 
-        for (autoscale_, team) in results {
-            autoscale.combine(&autoscale_);
+        let autoscale = autoscale_global.lock().unwrap();
+        let team_scores = score(&team);
+        let team_score = autoscale.scale(team_scores);
+        let best_score = autoscale.scale(score(&best_team.lock().unwrap()));
 
-            let team_scores = score(&team);
-            let team_score = autoscale.scale(team_scores);
-            let best_score = autoscale.scale(score(&best_team));
-
-            if team_score > best_score {
-                print!("Global best: {team_scores:4.2?} ");
-                team.iter()
-                    .map(|p| &p.species)
-                    .sorted()
-                    .for_each(|p| print!("{:?} ", p));
-                println!("{}", serde_json::to_string(&best_team).unwrap());
-                println!();
-                best_team = team.into();
-            } else {
-                // print!("Rejected: {scores:4.2?} ");
-                // team.iter().for_each(|p| print!("{:?} ", p.species));
-                // println!();
-            }
+        if team_score > best_score {
+            print!("Global best: {team_scores:4.2?} ");
+            team.iter()
+                .map(|p| &p.species)
+                .sorted()
+                .for_each(|p| print!("{:?} ", p));
+            println!("{}", serde_json::to_string(&*best_team.lock().unwrap()).unwrap());
+            println!();
+            best_team.lock().unwrap().clone_from(&team);
+        } else {
+            // print!("Rejected: {scores:4.2?} ");
+            // team.iter().for_each(|p| print!("{:?} ", p.species));
+            // println!();
         }
-    }
+    })
 }
